f016524a9d3ecd4304f4a953bc7fdbbc712a0068
Aveson <avesoncarvalho25@gmail.com>
2023-10-19 22:12:09 +0100

    added 3 pages, css and scripts. Gave all a netflix default css and js

---
+++ scripts/netflix.js
@@ -0,0 +1,20 @@
+// --------------- Import api key ---------------
+import * as apikey from "./apikey.js";
+
+// --------------- Displaying data ---------------
+let url = "https://watchmode.p.rapidapi.com/list-titles/?types=movie%2Ctv_series&page=5&release_date_start=20010101&release_date_end=20201211";
+let options = apikey.options;
+
+// displays the given data
+function displayData() {
+	fetch(url, options)
+		.then((data) => data.json())
+		.then((data) => {
+			console.log(data);
+		});
+}
+
+// displayData();
+
+// --------------- Extra attributes ---------------
+document.querySelector(".navbar-brand").setAttribute("draggable", false);
