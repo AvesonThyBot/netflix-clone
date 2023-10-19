b7168890dd3ed463c48c8004e27fd6dc9648c4c2
Aveson <avesoncarvalho25@gmail.com>
2023-10-19 20:12:03 +0100

    removed unnecessary files, added api folder and added api key details in apikey.js

---
+++ scripts/apikey.js
@@ -0,0 +1,10 @@
+const url = "https://watchmode.p.rapidapi.com/";
+const options = {
+	method: "GET",
+	headers: {
+		"X-RapidAPI-Key": "e504a28c16mshbfada8e425bd22dp1fb891jsnbc74c19c62e8",
+		"X-RapidAPI-Host": "watchmode.p.rapidapi.com",
+	},
+};
+
+export { options, url };
