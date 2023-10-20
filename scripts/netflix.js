90ee7c8186262ff8cc24e9f467d3a55d469ab517
Aveson <avesoncarvalho25@gmail.com>
2023-10-20 21:40:13 +0100

    make everything work like before, but less cluttered

--- scripts/netflix.js
+++ scripts/netflix.js
@@ -18,6 +18,35 @@ function displayData() {
 
 // --------------- Event listeners ---------------
 
+// display sections event listener
+document.addEventListener("DOMContentLoaded", function () {
+	const sections = document.querySelectorAll(".navbar-sections");
+	sections.forEach((section) => {
+		section.addEventListener("click", function () {
+			// change title of web
+			document.title = `${section.textContent} - Netflix`;
+			// remove active
+			sections.forEach((section) => {
+				section.classList.remove("active");
+			});
+			// add active
+			section.classList.add("active");
+			// display correct secion
+			const netflixSection = document.querySelectorAll(".netflix-sections");
+			// hide all previous netflix sections
+			netflixSection.forEach((section) => {
+				section.setAttribute("hidden", "hidden");
+			});
+			let selectedSection = section.textContent;
+			// format the name
+			selectedSection = `${selectedSection.toLowerCase().replace(/\s+/g, "-")}-section`;
+			selectedSection = document.querySelector(`.${selectedSection}`);
+			// unhide the picked section
+			selectedSection.removeAttribute("hidden", "hidden");
+		});
+	});
+});
+
 // btn-volume event listener
 const btnVolume = document.querySelector(".btn-volume");
 btnVolume.onclick = () => {
