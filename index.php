5dc0fa08f9d43e90d05cab28526f509cd61eae74
Aveson <avesoncarvalho25@gmail.com>
2023-10-28 12:38:05 +0100

    copypasted main display html into series and films

--- index.php
+++ index.php
@@ -5,8 +5,6 @@ if (count($_COOKIE) == 0) {
 }
 ?>
 
-
-
 <!-- Header -->
 <?php include "inc/header.php"; ?>
 		<!-- Main netflix display -->
@@ -181,13 +179,251 @@ if (count($_COOKIE) == 0) {
 		<!-- Series section -->
 		<section class="series-section netflix-sections" hidden>
 			<div class="series-display">
-				
+				<!-- Popular On Netflix -->
+				<div class="popular-series">
+					<h2>Popular On Netflix</h2>
+					<div class="scroll-container">
+						<!-- Scroll box -->
+						<button class="scroll-button scrollLeft">
+							<svg fill="#fff" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+								<g data-name="Layer 2">
+									<g data-name="arrow-ios-back">
+										<rect width="24" height="24" transform="rotate(90 12 12)" opacity="0" />
+
+										<path d="M13.83 19a1 1 0 0 1-.78-.37l-4.83-6a1 1 0 0 1 0-1.27l5-6a1 1 0 0 1 1.54 1.28L10.29 12l4.32 5.36a1 1 0 0 1-.78 1.64z" />
+									</g>
+								</g>
+							</svg>
+						</button>
+						<div class="scrollable-div popular-box"></div>
+						<button class="scroll-button scrollRight">
+							<svg fill="#fff" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+								<g data-name="Layer 2">
+									<g data-name="arrow-ios-forward">
+										<rect width="24" height="24" transform="rotate(-90 12 12)" opacity="0" />
+
+										<path d="M10 19a1 1 0 0 1-.64-.23 1 1 0 0 1-.13-1.41L13.71 12 9.39 6.63a1 1 0 0 1 .15-1.41 1 1 0 0 1 1.46.15l4.83 6a1 1 0 0 1 0 1.27l-5 6A1 1 0 0 1 10 19z" />
+									</g>
+								</g>
+							</svg>
+						</button>
+					</div>
+				</div>
+				<!-- Trending Now -->
+				<div class="trending-series">
+					<h2>Trending Now</h2>
+					<div class="scroll-container">
+						<!-- Scroll box -->
+						<button class="scroll-button scrollLeft">
+							<svg fill="#fff" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+								<g data-name="Layer 2">
+									<g data-name="arrow-ios-back">
+										<rect width="24" height="24" transform="rotate(90 12 12)" opacity="0" />
+
+										<path d="M13.83 19a1 1 0 0 1-.78-.37l-4.83-6a1 1 0 0 1 0-1.27l5-6a1 1 0 0 1 1.54 1.28L10.29 12l4.32 5.36a1 1 0 0 1-.78 1.64z" />
+									</g>
+								</g>
+							</svg>
+						</button>
+						<div class="scrollable-div trending-box"></div>
+						<button class="scroll-button scrollRight">
+							<svg fill="#fff" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+								<g data-name="Layer 2">
+									<g data-name="arrow-ios-forward">
+										<rect width="24" height="24" transform="rotate(-90 12 12)" opacity="0" />
+
+										<path d="M10 19a1 1 0 0 1-.64-.23 1 1 0 0 1-.13-1.41L13.71 12 9.39 6.63a1 1 0 0 1 .15-1.41 1 1 0 0 1 1.46.15l4.83 6a1 1 0 0 1 0 1.27l-5 6A1 1 0 0 1 10 19z" />
+									</g>
+								</g>
+							</svg>
+						</button>
+					</div>
+				</div>
+				<!-- Playing currently -->
+				<div class="playing-series">
+					<h2>Series</h2>
+					<div class="scroll-container">
+						<!-- Scroll box -->
+						<button class="scroll-button scrollLeft">
+							<svg fill="#fff" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+								<g data-name="Layer 2">
+									<g data-name="arrow-ios-back">
+										<rect width="24" height="24" transform="rotate(90 12 12)" opacity="0" />
+
+										<path d="M13.83 19a1 1 0 0 1-.78-.37l-4.83-6a1 1 0 0 1 0-1.27l5-6a1 1 0 0 1 1.54 1.28L10.29 12l4.32 5.36a1 1 0 0 1-.78 1.64z" />
+									</g>
+								</g>
+							</svg>
+						</button>
+						<div class="scrollable-div tv-box"></div>
+						<button class="scroll-button scrollRight">
+							<svg fill="#fff" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+								<g data-name="Layer 2">
+									<g data-name="arrow-ios-forward">
+										<rect width="24" height="24" transform="rotate(-90 12 12)" opacity="0" />
+
+										<path d="M10 19a1 1 0 0 1-.64-.23 1 1 0 0 1-.13-1.41L13.71 12 9.39 6.63a1 1 0 0 1 .15-1.41 1 1 0 0 1 1.46.15l4.83 6a1 1 0 0 1 0 1.27l-5 6A1 1 0 0 1 10 19z" />
+									</g>
+								</g>
+							</svg>
+						</button>
+					</div>
+				</div>
+				<!-- Upcoming Films -->
+				<div class="upcoming-series">
+					<h2>Films</h2>
+					<div class="scroll-container">
+						<!-- Scroll box -->
+						<button class="scroll-button scrollLeft">
+							<svg fill="#fff" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+								<g data-name="Layer 2">
+									<g data-name="arrow-ios-back">
+										<rect width="24" height="24" transform="rotate(90 12 12)" opacity="0" />
+
+										<path d="M13.83 19a1 1 0 0 1-.78-.37l-4.83-6a1 1 0 0 1 0-1.27l5-6a1 1 0 0 1 1.54 1.28L10.29 12l4.32 5.36a1 1 0 0 1-.78 1.64z" />
+									</g>
+								</g>
+							</svg>
+						</button>
+						<div class="scrollable-div films-box"></div>
+						<button class="scroll-button scrollRight">
+							<svg fill="#fff" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+								<g data-name="Layer 2">
+									<g data-name="arrow-ios-forward">
+										<rect width="24" height="24" transform="rotate(-90 12 12)" opacity="0" />
+
+										<path d="M10 19a1 1 0 0 1-.64-.23 1 1 0 0 1-.13-1.41L13.71 12 9.39 6.63a1 1 0 0 1 .15-1.41 1 1 0 0 1 1.46.15l4.83 6a1 1 0 0 1 0 1.27l-5 6A1 1 0 0 1 10 19z" />
+									</g>
+								</g>
+							</svg>
+						</button>
+					</div>
+				</div>
 			</div>
 		</section>
 		<!-- Film sections -->
 		<section class="films-section netflix-sections" hidden>
 			<div class="films-display">
-				
+				<!-- Popular On Netflix -->
+				<div class="popular-films">
+					<h2>Popular On Netflix</h2>
+					<div class="scroll-container">
+						<!-- Scroll box -->
+						<button class="scroll-button scrollLeft">
+							<svg fill="#fff" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+								<g data-name="Layer 2">
+									<g data-name="arrow-ios-back">
+										<rect width="24" height="24" transform="rotate(90 12 12)" opacity="0" />
+
+										<path d="M13.83 19a1 1 0 0 1-.78-.37l-4.83-6a1 1 0 0 1 0-1.27l5-6a1 1 0 0 1 1.54 1.28L10.29 12l4.32 5.36a1 1 0 0 1-.78 1.64z" />
+									</g>
+								</g>
+							</svg>
+						</button>
+						<div class="scrollable-div popular-box"></div>
+						<button class="scroll-button scrollRight">
+							<svg fill="#fff" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+								<g data-name="Layer 2">
+									<g data-name="arrow-ios-forward">
+										<rect width="24" height="24" transform="rotate(-90 12 12)" opacity="0" />
+
+										<path d="M10 19a1 1 0 0 1-.64-.23 1 1 0 0 1-.13-1.41L13.71 12 9.39 6.63a1 1 0 0 1 .15-1.41 1 1 0 0 1 1.46.15l4.83 6a1 1 0 0 1 0 1.27l-5 6A1 1 0 0 1 10 19z" />
+									</g>
+								</g>
+							</svg>
+						</button>
+					</div>
+				</div>
+				<!-- Trending Now -->
+				<div class="trending-films">
+					<h2>Trending Now</h2>
+					<div class="scroll-container">
+						<!-- Scroll box -->
+						<button class="scroll-button scrollLeft">
+							<svg fill="#fff" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+								<g data-name="Layer 2">
+									<g data-name="arrow-ios-back">
+										<rect width="24" height="24" transform="rotate(90 12 12)" opacity="0" />
+
+										<path d="M13.83 19a1 1 0 0 1-.78-.37l-4.83-6a1 1 0 0 1 0-1.27l5-6a1 1 0 0 1 1.54 1.28L10.29 12l4.32 5.36a1 1 0 0 1-.78 1.64z" />
+									</g>
+								</g>
+							</svg>
+						</button>
+						<div class="scrollable-div trending-box"></div>
+						<button class="scroll-button scrollRight">
+							<svg fill="#fff" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+								<g data-name="Layer 2">
+									<g data-name="arrow-ios-forward">
+										<rect width="24" height="24" transform="rotate(-90 12 12)" opacity="0" />
+
+										<path d="M10 19a1 1 0 0 1-.64-.23 1 1 0 0 1-.13-1.41L13.71 12 9.39 6.63a1 1 0 0 1 .15-1.41 1 1 0 0 1 1.46.15l4.83 6a1 1 0 0 1 0 1.27l-5 6A1 1 0 0 1 10 19z" />
+									</g>
+								</g>
+							</svg>
+						</button>
+					</div>
+				</div>
+				<!-- Playing currently -->
+				<div class="playing-films">
+					<h2>Series</h2>
+					<div class="scroll-container">
+						<!-- Scroll box -->
+						<button class="scroll-button scrollLeft">
+							<svg fill="#fff" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+								<g data-name="Layer 2">
+									<g data-name="arrow-ios-back">
+										<rect width="24" height="24" transform="rotate(90 12 12)" opacity="0" />
+
+										<path d="M13.83 19a1 1 0 0 1-.78-.37l-4.83-6a1 1 0 0 1 0-1.27l5-6a1 1 0 0 1 1.54 1.28L10.29 12l4.32 5.36a1 1 0 0 1-.78 1.64z" />
+									</g>
+								</g>
+							</svg>
+						</button>
+						<div class="scrollable-div tv-box"></div>
+						<button class="scroll-button scrollRight">
+							<svg fill="#fff" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+								<g data-name="Layer 2">
+									<g data-name="arrow-ios-forward">
+										<rect width="24" height="24" transform="rotate(-90 12 12)" opacity="0" />
+
+										<path d="M10 19a1 1 0 0 1-.64-.23 1 1 0 0 1-.13-1.41L13.71 12 9.39 6.63a1 1 0 0 1 .15-1.41 1 1 0 0 1 1.46.15l4.83 6a1 1 0 0 1 0 1.27l-5 6A1 1 0 0 1 10 19z" />
+									</g>
+								</g>
+							</svg>
+						</button>
+					</div>
+				</div>
+				<!-- Upcoming Films -->
+				<div class="upcoming-films">
+					<h2>Films</h2>
+					<div class="scroll-container">
+						<!-- Scroll box -->
+						<button class="scroll-button scrollLeft">
+							<svg fill="#fff" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+								<g data-name="Layer 2">
+									<g data-name="arrow-ios-back">
+										<rect width="24" height="24" transform="rotate(90 12 12)" opacity="0" />
+
+										<path d="M13.83 19a1 1 0 0 1-.78-.37l-4.83-6a1 1 0 0 1 0-1.27l5-6a1 1 0 0 1 1.54 1.28L10.29 12l4.32 5.36a1 1 0 0 1-.78 1.64z" />
+									</g>
+								</g>
+							</svg>
+						</button>
+						<div class="scrollable-div films-box"></div>
+						<button class="scroll-button scrollRight">
+							<svg fill="#fff" width="800px" height="800px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+								<g data-name="Layer 2">
+									<g data-name="arrow-ios-forward">
+										<rect width="24" height="24" transform="rotate(-90 12 12)" opacity="0" />
+
+										<path d="M10 19a1 1 0 0 1-.64-.23 1 1 0 0 1-.13-1.41L13.71 12 9.39 6.63a1 1 0 0 1 .15-1.41 1 1 0 0 1 1.46.15l4.83 6a1 1 0 0 1 0 1.27l-5 6A1 1 0 0 1 10 19z" />
+									</g>
+								</g>
+							</svg>
+						</button>
+					</div>
+				</div>
 			</div>
 		</section>
 		<!-- My list section -->
