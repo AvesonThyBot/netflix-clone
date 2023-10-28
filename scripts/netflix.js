884fe61ed77452ffcd9367680cafd7c824a22bb1
Aveson <avesoncarvalho25@gmail.com>
2023-10-28 12:28:48 +0100

    run function when the certain page is called, instead of preloading

--- scripts/netflix.js
+++ scripts/netflix.js
@@ -61,6 +61,7 @@ if (location.href.includes("?page_type")) {
 	document.title = `Home - Netflix`;
 	document.querySelector(".home-navbar-section").classList.add("active");
 	document.querySelector(".home-section").removeAttribute("hidden");
+	displayMain();
 }
 
 // --------------- All displaying function ---------------
@@ -197,10 +198,10 @@ function displaySeries() {
 		.then((data) => data.json())
 		.then((data) => {});
 }
-
-displayMain();
-displayMovies();
-displaySeries();
+// ------- My list display -------
+function displayMyList() {
+	console.log("hey");
+}
 // --------------- Event listeners ---------------
 // display sections event listener
 document.addEventListener("DOMContentLoaded", function () {
@@ -231,6 +232,21 @@ document.addEventListener("DOMContentLoaded", function () {
 				document.querySelector(".hero-video").muted = true;
 				const volumeMute = `<svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" class="ltr-4z3qvp e1svuwfo1" data-name="VolumeOff" aria-hidden="true"><path fill-rule="evenodd" clip-rule="evenodd" d="M11 4.00003C11 3.59557 10.7564 3.23093 10.3827 3.07615C10.009 2.92137 9.57889 3.00692 9.29289 3.29292L4.58579 8.00003H1C0.447715 8.00003 0 8.44774 0 9.00003V15C0 15.5523 0.447715 16 1 16H4.58579L9.29289 20.7071C9.57889 20.9931 10.009 21.0787 10.3827 20.9239C10.7564 20.7691 11 20.4045 11 20V4.00003ZM5.70711 9.70714L9 6.41424V17.5858L5.70711 14.2929L5.41421 14H5H2V10H5H5.41421L5.70711 9.70714ZM15.2929 9.70714L17.5858 12L15.2929 14.2929L16.7071 15.7071L19 13.4142L21.2929 15.7071L22.7071 14.2929L20.4142 12L22.7071 9.70714L21.2929 8.29292L19 10.5858L16.7071 8.29292L15.2929 9.70714Z" fill="currentColor"></path></svg>`;
 				document.querySelector(".btn-volume").innerHTML = volumeMute;
+			}
+			// Run certain function depending on page
+			switch (selectedSection.classList[0]) {
+				case "home-section":
+					displayMain();
+					break;
+				case "series-section":
+					displaySeries();
+					break;
+				case "films-section":
+					displayMovies();
+					break;
+				case "my-list-section":
+					displayMyList();
+					break;
 			}
 		});
 	});
